a9d05aef2b7b1a685449f81a6d391e54ca05d029
Goto <mateusgoto01@gmail.com>
2021-12-06 14:17:13 -0300

    [Changes] h modified

--- metodo_de_RungeKutta.py
+++ metodo_de_RungeKutta.py
@@ -15,12 +15,12 @@ xf = 1  # final no intervalo
 
 n = (int)(xf / h)
 print(n)
-x_array = np.empty(n)
+x_array = np.empty(n+1)
 x_array[0] = 0
 y_array = np.copy(x_array)
 y_array[0] = 1
 
-for i in range(n-1):
+for i in range(n):
     k1 = h*f(x_array[i], y_array[i])
     k2 = h*f(x_array[i]+(h/2), y_array[i] + k1/2)
     k3 = h * f(x_array[i] + (h/2), y_array[i] + (k2/2))
@@ -29,3 +29,4 @@ for i in range(n-1):
     y_array[i + 1] = y_array[i] + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
 
 print(y_array)
+print(x_array)
